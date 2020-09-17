9fbf70c27e658a49fc59de0c6a8291477bb73633
Eduardo Lima <dlima78@hotmail.com>
2020-09-17 11:15:26 -0300

    feat: ensure AccountMongoRepository returns account on success

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
-      version: '4.0.3',
+      version: '3.6.4',
       skipMD5: true
     },
+    autoStart: false,
     instance: {
       dbName: 'jest'
-    },
-    autoStart: false
+    }
   }
 }
--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,6 @@ module.exports = {
   roots: [
     '<rootDir>/src'
   ],
-  // testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
