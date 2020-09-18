e397ef9903ca419359a262d53754065ff3022dc9
Eduardo Lima <dlima78@hotmail.com>
2020-09-18 13:42:19 -0300

    refactor: make server error return error stack

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,6 +1,7 @@
 export class ServerError extends Error {
-  constructor () {
+  constructor (stack: string) {
     super('Aconteceu um erro inexperado tente mais tarde')
     this.name = 'ServerError'
+    this.stack = stack
   }
 }
