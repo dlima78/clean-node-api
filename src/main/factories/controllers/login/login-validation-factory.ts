a2ef4fef7e585f2ab8457affaca61f4c7016a153
Eduardo Lima <dlima78@hotmail.com>
2020-10-05 15:46:46 -0300

    refactor: move validator to its own layer

--- src/main/factories/controllers/login/login-validation-factory.ts
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -1,10 +1,10 @@
+import { Validation } from '../../../../presentation/protocols'
 import {
   EmailValidation,
   RequiredFieldValidation,
   ValidationComposite
-} from '../../../../presentation/helper/validators'
-import { Validation } from '../../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../adapters/validators/email-validator-adapter'
+} from '../../../../validation/validators'
+import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
 
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = []
--- src/main/factories/controllers/signup/signup-validation-factory.spec.ts
+++ src/main/factories/controllers/signup/signup-validation-factory.spec.ts
@@ -3,12 +3,12 @@ import {
   CompareFieldsValidation,
   RequiredFieldValidation,
   ValidationComposite
-} from '../../../../presentation/helper/validators'
+} from '../../../../validation/validators'
 import { Validation } from '../../../../presentation/protocols/validation'
-import { EmailValidator } from '../../../../presentation/protocols/email-validator'
+import { EmailValidator } from '../../../../validation/protocols/email-validator'
 import { makeSignupValidation } from './signup-validation-factory'
 
-jest.mock('../../../../presentation/helper/validators/validation-composite')
+jest.mock('../../../../validation/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
