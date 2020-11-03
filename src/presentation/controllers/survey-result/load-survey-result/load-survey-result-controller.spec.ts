eb08622701ab38631147938a60094b24e66caa10
Eduardo Lima <dlima78@hotmail.com>
2020-11-03 09:50:19 -0300

    test: ensure LoadSurveyResultController calls LoadSurveyById with correct id

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -0,0 +1,44 @@
+import { LoadSurveyResultController } from './load-survey-result-controller'
+import {
+  HttpRequest,
+  LoadSurveyById
+} from './load-survey-result-controller-protocols'
+import MockDate from 'mockdate'
+import { mockLoadSurveyById } from '@/presentation/test'
+
+const makeFakeRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_id'
+  }
+})
+
+type SutTypes = {
+  sut: LoadSurveyResultController
+  loadSurveyByIdStub: LoadSurveyById
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = mockLoadSurveyById()
+  const sut = new LoadSurveyResultController(loadSurveyByIdStub)
+  return {
+    sut,
+    loadSurveyByIdStub
+  }
+}
+
+describe('LoadSurveyResultController', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('should call LoadSurveyById with correct value', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    const loadByIdSpy = spyOn(loadSurveyByIdStub, 'loadById')
+    await sut.handle(makeFakeRequest())
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+})
